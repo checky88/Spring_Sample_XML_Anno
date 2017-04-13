722fd1fb0b2f38b65217daceefb6c5b3d6d9bb17
checky88 <James1988>
2017-04-13 14:38:22 +0100

    All

---
+++ src/main/java/com/pluralsight/model/Customer.java
@@ -0,0 +1,28 @@
+package com.pluralsight.model;
+
+public class Customer {
+
+	private String firstname;
+	private String lastnamename;
+	
+	public Customer(){
+		
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastnamename() {
+		return lastnamename;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public void setLastnamename(String lastnamename) {
+		this.lastnamename = lastnamename;
+	}
+
+}
